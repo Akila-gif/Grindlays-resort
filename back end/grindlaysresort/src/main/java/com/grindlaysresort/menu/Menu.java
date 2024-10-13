c089339c5236e7b0c0766dc9d4cf2f0e2cf24e1f
Akila-gif <60591310+Akila-gif@users.noreply.github.com>
2024-10-13 20:19:59 +0530

    Complieate get all data request handeling

---
+++ back end/grindlaysresort/src/main/java/com/grindlaysresort/menu/Menu.java
@@ -0,0 +1,26 @@
+package com.grindlaysresort.menu;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(
+        name = "menu"
+)
+public class Menu {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @Column(name = "name")
+    String name;
+
+
+}
---
+++ back end/grindlaysresort/src/main/java/com/grindlaysresort/payment/Payment.java
@@ -0,0 +1,36 @@
+package com.grindlaysresort.payment;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.grindlaysresort.reservation.Reservation;
+import com.grindlaysresort.reservation.ReservationState;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(
+        name = "payment"
+)
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Integer id;
+
+    @Column(name = "totalpayment")
+    BigDecimal totalpayment;
+
+    @Column(name = "paidamount")
+    BigDecimal paidamount;
+
+    @ManyToMany(mappedBy = "Payment_id")
+    @JsonIgnore
+    List<Reservation> reservations;
+}
